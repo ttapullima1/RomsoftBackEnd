0a0d515a097b760e94a161fd62b21880fe453bed
ttapullimaa <ttapullima@outlook.com>
2021-02-03 00:37:42 -0500

    Actualizado 03022021

--- Romsoft.GESTIONCLINICA.DataAccess/Interfaces/ICVN_TARIFARIO_SEGUSRepository/ICVN_TARIFARIO_SEGUSRepository.cs
+++ Romsoft.GESTIONCLINICA.DataAccess/Interfaces/ICVN_TARIFARIO_SEGUSRepository/ICVN_TARIFARIO_SEGUSRepository.cs
@@ -1,9 +1,4 @@
 ﻿using Romsoft.GESTIONCLINICA.DataAccess.Core;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Romsoft.GESTIONCLINICA.DataAccess.Interfaces.ICVN_TARIFARIO_SEGUSRepository
 {
